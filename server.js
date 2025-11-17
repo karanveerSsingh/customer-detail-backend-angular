ea64e2c39e7eb0a9c8770db38e78ce0aa1e87e02
karanveerSsingh <karanveer0508singh@gmail.com>
2025-11-17 21:08:09 +0530

    paths

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE",
+  })
+);
 app.use(bodyParser.json());
 require("dotenv").config();
 
